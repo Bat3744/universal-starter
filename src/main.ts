df76a1240b7892fc2f7e461973b8178e6038f170
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2017-12-07 14:35:41 +0100

    Fix bootstrap JS

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import 'jquery';
+import 'bootstrap';
+
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule);
+    platformBrowserDynamic().bootstrapModule(AppModule);
 });
