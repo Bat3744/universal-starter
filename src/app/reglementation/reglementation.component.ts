216bdfdfa8afdd7f61282a6ebc11d88dd596c878
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2017-12-07 14:10:05 +0100

    Fix SVG import

--- src/app/reglementation/reglementation.component.ts
+++ src/app/reglementation/reglementation.component.ts
@@ -11,6 +11,6 @@ export class ReglementationComponent {
 
 	constructor(private sanitized: DomSanitizer) {}
 
-	diagramEssai1SVG = this.sanitized.bypassSecurityTrustHtml(require('../../assets/images/diag_essais_1.svg'));
-	diagramEssai2SVG = this.sanitized.bypassSecurityTrustHtml(require('../../assets/images/diag_essais_2.svg'));
+	diagramEssai1SVG = require('../../assets/images/diag_essais_1.svg');
+	diagramEssai2SVG = require('../../assets/images/diag_essais_2.svg');
 }
